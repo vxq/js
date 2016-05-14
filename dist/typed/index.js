6e7fa292ae2c4b0619ad9ebf4da28728e9189734
vxq <vxq@vxq.ca>
2016-05-14 04:00:35 -0700

    This is the worst of all worlds.

--- dist/typed/index.js
+++ dist/typed/index.js
@@ -3,7 +3,6 @@ const turtles = require('./turtles.ts');
 const assert = require('./assert.ts');
 const testTheTurtles = () => {
     const turtle = new turtles.Turtle;
-    turtle.foo = 4;
     assert.equals(0, turtle.x);
 };
 testTheTurtles();
--- vxq/index.ts
+++ vxq/index.ts
@@ -4,7 +4,6 @@ const assert = require('./assert.ts');
 
 const testTheTurtles = () => {
   const turtle = new turtles.Turtle;
-  turtle.foo = 4;
   assert.equals(0, turtle.x);
 };
 
