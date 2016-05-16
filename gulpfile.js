04c6c02d91141aba74c026799ca85dab3714f3cb
vxq <vxq@vxq.ca>
2016-05-15 22:22:29 -0700

    Promoting missing return statements to errors, and other changes.

--- gulpfile.js
+++ gulpfile.js
@@ -27,26 +27,25 @@ const flags = {
   language_out: 'ECMASCRIPT5_STRICT',
   entry_point: 'vxq.main',
   generate_exports: true,
-  jscomp_error: ['checkTypes'],
+  jscomp_error: ['checkTypes', 'missingReturn'],
   externs: [
     'vxq/public.externs.js',
     'vxq/environment.externs.js'
   ]
 };
 
-gulp.task('build', ['build-simple', 'build-debug', 'build-prod']);
-gulp.task('build-seq', () => runSequence('build-simple', 'build-debug', 'build-prod'));
+gulp.task('build', () => runSequence('build-simple', 'build-debug', 'build-prod'));
 
-gulp.task('build-simple', () => {
+gulp.task('build-simple', () =>
   gulp.src(srcs).pipe(closureCompiler(Object.assign({}, flags, {
     compilation_level: 'SIMPLE_OPTIMIZATIONS',
     js_output_file: 'simple.js',
     jscomp_warning: ['checkTypes'],
+    jscomp_error: [],
     debug: true,
     formatting: 'pretty_print',
     define: ['vxq.debug.DEBUG=true']
-  }))).pipe(gulp.dest('zdist'));
-});
+  }))).pipe(gulp.dest('zdist')));
 
 gulp.task('build-debug', () =>
   gulp.src(srcs).pipe(closureCompiler(Object.assign({}, flags, {
