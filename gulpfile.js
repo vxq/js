9ee2877f68663b5880e9cf3e2cf81cf2b1ced4a4
vxq <vxq@vxq.ca>
2016-05-16 20:13:48 -0700

    Use closure-compiler's built-in linter instead of gjslint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const closureCompiler = require('google-closure-compiler').gulp();
-const gjslint = require('gulp-gjslint');
 const runSequence = require('run-sequence');
 
 
@@ -27,21 +26,23 @@ const flags = {
   language_out: 'ECMASCRIPT5_STRICT',
   entry_point: 'vxq.main',
   generate_exports: true,
-  jscomp_error: ['checkTypes'],
+  jscomp_error: ['checkTypes', 'missingReturn'],
+  jscomp_warning: [],
   externs: [
     'vxq/public.externs.js',
     'vxq/environment.externs.js'
   ]
 };
 
-gulp.task('build', ['build-simple', 'build-debug', 'build-prod']);
-gulp.task('build-seq', () => runSequence('build-simple', 'build-debug', 'build-prod'));
+gulp.task('pbuild', ['build-simple', 'build-debug', 'build-prod']);
+gulp.task('build', () => runSequence('build-simple', 'build-debug', 'build-prod'));
 
 gulp.task('build-simple', () => {
   gulp.src(srcs).pipe(closureCompiler(Object.assign({}, flags, {
     compilation_level: 'SIMPLE_OPTIMIZATIONS',
     js_output_file: 'simple.js',
     jscomp_warning: ['checkTypes'],
+    jscomp_error: [],
     debug: true,
     formatting: 'pretty_print',
     define: ['vxq.debug.DEBUG=true']
@@ -62,12 +63,11 @@ gulp.task('build-prod', () =>
   }))).pipe(gulp.dest('zdist')));
 
 gulp.task('lint', () =>
-  gulp.src('vxq/**/*.js')
-    .pipe(gjslint())
-    .pipe(gjslint.reporter('console'))
-    .pipe(gjslint.reporter('fail')));
-
-gulp.task('lint-full', () =>
-  gulp.src('vxq/**/*.js')
-    .pipe(gjslint())
-    .pipe(gjslint.reporter('console')));
+  // Runs prod build, but with lint checks and all warnings turned to errors,
+  // and discarding the output files.
+  gulp.src(srcs).pipe(closureCompiler(Object.assign({}, flags, {
+    js_output_file: 'prod.js',
+    jscomp_error:
+        flags.jscomp_error.concat(['lintChecks', ...flags.jscomp_warning]),
+    jscomp_warning: []
+  }))).pipe(gulp.dest('/dev/null')));
